ba1b8488dc1ad10e85d542f92e9b8bc39eb86d4c
Akshay Agrawal <aagrawal@isystango.com>
2020-11-12 18:45:42 +0530

    minor

--- users/schema.py
+++ users/schema.py
@@ -24,9 +24,8 @@ from .models import Profile
 from graphene_django.filter.fields import DjangoFilterConnectionField
 
 class Query(graphene.ObjectType):
-    # this defines a Field `hello` in our Schema with a single Argument `name`
+    # hello query to check is_authenticated or not
     hello = graphene.String(name=graphene.String(default_value="stranger"))
-    goodbye = graphene.String()
     user_by_id = graphene.Field(UserType, id=graphene.Int())
     profile_by_id = graphene.Field(ProfileType, id=graphene.Int())
     users = DjangoFilterConnectionField(UserType)
@@ -35,14 +34,8 @@ class Query(graphene.ObjectType):
     # our Resolver method takes the GraphQL context (root, info) as well as
     # Argument (name) for the Field and returns data for the query Response
     def resolve_hello(root, info, name):
-        is_verified = info.context.user.is_authenticated 
-        return f'Hello {name} you are logged in : {is_verified}'
-
-    def resolve_goodbye(root, info):
-        return 'See ya!'
-
-    def resolve_users(self, info, **kwargs):
-        return User.objects.all()
+        is_authenticated = info.context.user.is_authenticated 
+        return f'Hello {name} you are logged in : {is_authenticated}'
 
     def resolve_user_by_id(root, info, id):
         return User.objects.get(id=id)
